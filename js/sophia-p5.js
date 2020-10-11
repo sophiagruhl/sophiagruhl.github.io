ce6868405c3b4d11cc146f77536f596bd5dac3e5
sophiagruhl <70415887+sophiagruhl@users.noreply.github.com>
2020-10-10 17:34:17 -0700

    Update sophia-p5.js

--- js/sophia-p5.js
+++ js/sophia-p5.js
@@ -1,1 +1,28 @@
 
+function preload() {
+    
+}
+
+function setup() {
+  createCanvas(1000, 1000); 
+  background(255,255,255);
+  textSize(32);
+  text('word', 10, 30);
+  fill(0, 102, 153, 51);
+}
+
+function draw() {
+    
+}
+
+function keyPressed() {
+  if (keyCode === 74) {
+    
+  } else if (keyCode === 76) {
+    
+  }
+}
+
+function mousePressed() {
+    y = 90;
+}
